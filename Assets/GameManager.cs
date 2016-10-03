274ae1f9a103efcca78f70ba6fd79e644b1de19e
Haemaru <rkglskame01@gmail.com>
2016-10-03 12:28:43 +0900

    1주차 과제

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    int score = 0;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddScore()
+    {
+        score++;
+        Debug.Log("Score : " + score);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject WallPrefab;
+    public float SpawnInterval = 3f;
+    public float YRange = 5f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            var ypos = Random.Range(-YRange, YRange);
+            Instantiate(WallPrefab, new Vector3(10, ypos, 0), Quaternion.identity);
+
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+}
